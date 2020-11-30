05e248a51bf8e79ca6037d0caaf300e9200d6fde
Alejandro Gonzalez <alexgve7sv@outlook.com>
2020-11-29 23:07:45 -0600

    spinner added

--- src/components/admin/inicioadmin.js
+++ src/components/admin/inicioadmin.js
@@ -18,12 +18,10 @@ const InicioAd = () => {
             setLoading(false)
         })
 
+        return () => unsuscribe()
+
     }, [])
 
-
-    
-
-    
 
     return (
         <>
@@ -49,7 +47,15 @@ const InicioAd = () => {
                         </div>
                     </ListGroup.Item>
                     { 
-                        isLoading ? <h1>Cargando</h1> : 
+                        isLoading ? 
+                        
+                        <div className="d-flex justify-content-center mt-5">
+                            <div class="spinner-border" role="status">
+                            <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+
+                        : 
 
                         news.map((element) => 
                         
